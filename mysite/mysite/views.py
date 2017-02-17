cf80d644def348ddff511f33225d694e5b068d78
svsrikanth2001 <emailsv@gmail.com>
2017-02-17 20:09:57 +0530

    15

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import HttpResponse
 from django.http import JsonResponse
 
+from models import * 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world Welcome to Python!!.")
 
 def booklist(request):
-    return JsonResponse({'key': 'value'}) 
+    all_books = []
+    for book in TblBook.objects.all():
+        all_books.append({
+            'name': book.BookName
+        })
+    return JsonResponse({'books': all_books})
 
 def tblBook(request):
 	 return JsonResponse({'key': 'value'}) 

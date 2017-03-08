3957e1cebd5737ef2e974b72f6a7a7216def3059
svsrikanth2001 <emailsv@gmail.com>
2017-03-08 23:32:04 +0530

    35

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,9 +122,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT =  'staticfiles'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )

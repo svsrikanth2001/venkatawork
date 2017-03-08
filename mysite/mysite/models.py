68d4dff95ef07459aeac4f84977f757ee8249dff
svsrikanth2001 <emailsv@gmail.com>
2017-03-08 22:25:59 +0530

    28

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class TblBook(models.Model):
     BookName = models.CharField(max_length=200)
+    def __unicode__(self):
+        return 'TblBook id: ' + str(self.id)
     
 class TblBookReview(models.Model):
 	tblbook = models.ForeignKey(TblBook, on_delete=models.CASCADE)
